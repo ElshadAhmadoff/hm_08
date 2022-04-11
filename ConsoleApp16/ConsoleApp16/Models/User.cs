84f88a1b8cee5fe103c64b8eb8e5b5aa5f32d32c
ElshadAhmadoff <744s72c@code.edu.az>
2022-04-11 08:08:54 +0400

    homework

---
+++ ConsoleApp16/ConsoleApp16/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace hm_08_04
+    {
+        public class User
+        {
+            public int userId { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
+        }
+    }
+}
---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp16.Models.hm_08_04;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace hm_08_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpClient client = new HttpClient();
+            string str = client.GetStringAsync("https://jsonplaceholder.typicode.com/posts").Result;
+            List<User> user = JsonConvert.DeserializeObject<List<User>>(str);
+            foreach (User item in user)
+            {
+                Console.WriteLine(item.title);
+            }
+
+        }
+    }
+}
